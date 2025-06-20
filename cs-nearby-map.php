3f52d5b2a97a9c3fdaade8a98cb1470c1b09f9c0
KornelLionstack <hauzer.kornel@lionstack.hu>
2025-06-20 13:23:46 +0200

    Load helper functions

--- cs-nearby-map.php
+++ cs-nearby-map.php
@@ -10,6 +10,11 @@
  * Domain Path: /languages
  */
 
+/* Load additional helper functions */
+if ( file_exists( plugin_dir_path( __FILE__ ) . 'functions.php' ) ) {
+    require_once plugin_dir_path( __FILE__ ) . 'functions.php';
+}
+
 if(!class_exists('CspmNearbyMap')){
 	
 	class CspmNearbyMap{
